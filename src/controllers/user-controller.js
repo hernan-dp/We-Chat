d2f58c9b018dd6666c9edf9bdd3b77ce197eaaaa
Hernan <h.de.prada1@gmail.com>
2020-02-10 03:49:14 -0300

    Creada mutacion de signup

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,3 +1,5 @@
+import jwt from 'jsonwebtoken'
+
 export const createUser = (_, { input }, { models }) => {
   return models.user.create(input)  
 }
@@ -14,4 +16,19 @@ export const findAllUsers = (obj, args, { models }) => {
 
 export const findUserById = (_, { id }, { models }) => {
   return models.user.findByPk(id)
+}
+
+export const signup = async (_, input, { models }) => {
+  const user = await models.user.findOne({ where: { username: input.username }}) 
+  if (user === null){
+    const user = await models.user.createUser({ ...input })
+    const token = jwt.sign({ sub: user.id }, APP_SECRET, { exp: '10d' } )
+    return {
+      token,
+      user,
+    }
+  }
+  else {
+    return console.error("Usuario ya existente");
+  }
 }
