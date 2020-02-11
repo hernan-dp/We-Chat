cd283647537e852c397800b107b3327fafde0180
Hernan <h.de.prada1@gmail.com>
2020-02-10 22:01:10 -0300

    SignUp Mutation

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -20,15 +20,16 @@ export const findUserById = (_, { id }, { models }) => {
 
 export const signup = async (_, input, { models }) => {
   const user = await models.user.findOne({ where: { username: input.username }}) 
-  if (user === null){
-    const user = await models.user.createUser({ ...input })
-    const token = jwt.sign({ sub: user.id }, APP_SECRET, { exp: '10d' } )
+  if (!user){
+    user = await models.user.createUser({ input })
+    const secret = process.env.JWT_SECRET
+    const token = jwt.sign({ sub: user.id }, secret, { exp: '10d' } )
     return {
       token,
       user,
     }
   }
   else {
-    return console.error("Usuario ya existente");
+    throw new Error('Usuario ya existente');
   }
 }
