5736875c3a82225b438cb7fbba99bb49593efc99
JoaGardiola <joa.gardiola@gmail.com>
2020-03-27 20:10:35 -0300

    Change input type on sendMessage mutation

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -54,7 +54,7 @@ export const userlogged = (_, id, { user }) => {
   return user
 }
 
-export const sendMessage = (_, { sender, channel, text}, { pubsub }) => {
+export const sendMessage = (_, {input: { sender, channel, text}}, { pubsub }) => {
   const message = { sender, channel, text}
   pubsub.publish(channel, { message })
   return message
